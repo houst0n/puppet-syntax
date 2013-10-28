3694609f630c6531f5ff509c06d9c9a0d2235a8e
Neil Houston <neil.a.houston@gmail.com>
2013-10-28 17:39:01 +0000

    Allow us to configure the parser we use for syntax checking

--- lib/puppet-syntax.rb
+++ lib/puppet-syntax.rb
@@ -6,9 +6,15 @@ require "puppet-syntax/hiera"
 module PuppetSyntax
   class << self
     attr_accessor :exclude_paths
+    attr_accessor :parser_version
 
     def exclude_paths
       @exclude_paths || []
     end
+
+    def parser_version
+      @parser_version || nil
+    end
+
   end
 end
